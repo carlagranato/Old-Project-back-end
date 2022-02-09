0dbc8020c930722e6fb072cd6fdbe79e5684b53a
radkeey <tiago_marchesini@hotmail.com>
2022-02-08 22:33:49 -0300

    Criação de componente Header e página Home

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const Header : FunctionComponent = () => {
+    return (
+        <>
+        </>
+    )
+}
+    
+
+export default Header
---
+++ front-end/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+import Header from "../components/Header";
+
+const Home : FunctionComponent = () => {
+    return (
+        <>
+            <Header>
+                
+            </Header>
+        </>
+        
+    )
+}
+
+export default Home
