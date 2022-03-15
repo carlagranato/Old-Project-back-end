824ed66343d4b9b65dc8b109234914eff16c9d27
radkeey <tiago_marchesini@hotmail.com>
2022-03-15 20:01:46 -0300

    Ajusta a configuração do CORs

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 
 // Cors
 app.use(cors({
-	origin: ['http://localhost:3000', 'https://old-project-front-end.herokuapp.com/']
+	origin: ['http://localhost:3000', 'https://old-project-front-end.herokuapp.com']
 }))
 
 // Endpoint para listar todas as categorias
